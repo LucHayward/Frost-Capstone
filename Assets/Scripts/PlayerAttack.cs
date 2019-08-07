0f4dbe8cfb2bf2d6c66a1615235d500c573fbd56
Luc Hayward <luchay10@gmail.com>
2019-08-07 11:55:30 +0200

    Added cannon and scripts from previous project

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float range = 25f;
+    public Transform skull;
+    public Transform origin;
+
+    public SpawnManager spawnManager;
+    
+    public ParticleSystem attackParticles;
+    public AudioSource attackClip;
+
+    private void Awake()
+    {
+        skull.gameObject.GetComponent<ParticleSystem>().Play();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(origin.position, origin.forward, out hit, range)){
+            Debug.DrawRay(origin.position, origin.forward, Color.green, 3);
+            Debug.Log(hit.transform.name);
+
+            skull.gameObject.GetComponent<ParticleSystem>().Play(true);
+
+            //If the object can be destroyed, play the attached particle system and 
+            // destroy it after that completes.
+            Debug.Log(hit.transform.tag);
+            if (hit.transform.CompareTag("Destructible"))
+            {
+                //Debug.Log("Destructible");
+                var exp = hit.transform.GetComponent<ParticleSystem>();
+                bool isBrazier = hit.transform.name.StartsWith("Brazier");
+                if(!isBrazier) exp.Play();
+                Destroy(hit.transform.gameObject, isBrazier ? 0 : exp.main.duration);
+
+                attackClip.Play();
+
+                spawnManager.SpawnNew();
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+    public Animator animator;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("IsJump", false);
+
+        if (characterController.isGrounded)
+        {
+
+            moveDirection = new Vector3();
+
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            forward *= Input.GetAxis("Vertical");
+
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            right *= Input.GetAxis("Horizontal");
+
+            moveDirection += forward;
+            moveDirection += right;
+            moveDirection *= speed;
+
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+                animator.SetBool("IsJump", true);
+            }
+        }
+        else
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            forward *= Input.GetAxis("Vertical") * speed;
+
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            right *= Input.GetAxis("Horizontal") * speed;
+
+            moveDirection.x = forward.x + right.x;
+            moveDirection.z = forward.z + right.z;
+            moveDirection.y -= gravity * Time.deltaTime;
+
+        }
+
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
+    }
+
+
+}
