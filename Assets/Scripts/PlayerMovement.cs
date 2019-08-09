74017f66ed4a645cdc75d90cd3a4d68304611083
Luc Hayward <luchay10@gmail.com>
2019-08-09 17:12:32 +0200

    Test commmit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,11 @@ public class PlayerMovement : MonoBehaviour
     CharacterController characterController;
     public Animator animator;
 
+    private Vector3 moveDirection;
+
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
-
-    private Vector3 moveDirection;
 
     void Start()
     {
