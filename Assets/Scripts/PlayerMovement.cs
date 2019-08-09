fd5f5aa456688db1433e66a87fe9a68af718bcfb
keegan337 <keeganthomaswhite@gmail.com>
2019-08-09 17:14:22 +0200

    test comment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,8 +9,9 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
+    public float peen1 = 0.0f;
     public float gravity = 20.0f;
-
+    public float peen = 0.0f;
     private Vector3 moveDirection;
 
     void Start()
