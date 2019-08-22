f0687e7c657e92b34ffaaaa97d04559ae6c3ffbe
Keegan WHite <keeganthomaswhite@gmail.com>
2019-08-23 01:51:44 +0200

    Flocking behaviour added
    Cohesion, alignment and avoidance scripts and objects created and added. Flock game object also created along with the flocking scripts.

---
+++ Assets/Scripts/AlignmentBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Alignment")]
+public class AlignmentBehaviour : FlockBehaviour
+{
+    /// <summary>
+    /// Finds the alignment that each agent should have in the flock
+    /// </summary>
+    /// <param name="agent"> the current agent </param>
+    /// <param name="context"> the list of neighbouring objects </param>
+    /// <param name="flock"> the current flock </param>
+    /// <returns> the ideal alignment </returns>
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        ///if there are no neighbours maintain parent alignment
+        if (context.Count == 0)
+            return agent.transform.forward;
+
+        ///add all points tgether and average
+        Vector3 alignmentMove = Vector3.zero;
+        foreach (Transform item in context)
+        {
+            alignmentMove += item.transform.forward;
+        }
+        alignmentMove /= context.Count;
+
+        return alignmentMove;
+    }
+}
