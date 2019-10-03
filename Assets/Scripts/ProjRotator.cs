931c3af32587f37bba414108461758d8373b4aa9
Jesse Mark <jessemark21@gmail.com>
2019-10-03 14:34:59 +0200

    Massive improvements to environment

--- Assets/Scripts/ProjRotator.cs
+++ Assets/Scripts/ProjRotator.cs
@@ -13,6 +13,6 @@ public class ProjRotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0.2f, 0.9f, 0);
+        transform.Rotate(0.0f, 0.9f, 0);
     }
 }
